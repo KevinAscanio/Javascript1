919edf75caa8d34a9d15e860c79437ec8f317bb6
Kevin Ascanio <kevinascanior@gmail.com>
2022-06-07 00:57:54 -0300

    Desafio Entregable 1.0

--- javas/miarchivo.js
+++ javas/miarchivo.js
@@ -1,19 +1,51 @@
-alert("Hola, Extraño.. Soy el Osito cariñoso!");
+let nombre = prompt("Hola, Como te llamas?");
 
-let nombre = prompt("Dime tu nombre");
-
-let abrazos = Number(
-  prompt(
-    "Dime cuantos abrazos quieres y te los daré:) (No mas de 3 porque no termino nunca D: ) "
-  )
-);
-
-if (abrazos !== 0) {
-  for (let abrasitos = 1; abrasitos <= abrazos; abrasitos++) {
-    alert(`${nombre} toma ${abrasitos} abrazitos`);
-  }
-} else {
-  alert("QUE MAL EDUCADO");
+if (typeof nombre != "string" || nombre == " " || nombre == "") {
+  nombre = prompt("No ingresaste un nombre valido. Intenta de nuevo.");
 }
 
-alert("Mision cumplida! Hasta luego!");
+alert(`Hola ${nombre} vamos a jugar piedra, papel o tijeras.`);
+
+let jugamos = true;
+
+function contar() {
+  alert("Okey aqui vamos.");
+  alert("Piedra...");
+  alert("Papel...");
+  alert("O tijeras.");
+  for (h = 1; h <= 3; h++) {
+    alert(h);
+  }
+}
+
+while (jugamos) {
+  let opciones = Number(
+    prompt(`Haz tu eleccion: 
+    1.Piedra 
+    2.Papel 
+    3.Tijeras 
+    4.No jugar`)
+  );
+
+  switch (opciones) {
+    case 1:
+      contar();
+      alert(`Tu elegiste piedra y yo elegi papel jaja..Suerte la proxima.`);
+      break;
+    case 2:
+      contar();
+      alert(`Tu elegiste papel y yo elegi papel jaja..Empate.`);
+      break;
+    case 3:
+      contar();
+      alert(`Tu elegiste tijeras y yo elegi papel me ganaste :( `);
+      break;
+    case 4:
+      alert(`Entiendo ${nombre}, será en otro momento. Adios!`);
+      jugamos = false;
+      break;
+    default:
+      alert("No elegiste un numero correcto, intenta de nuevo");
+      break;
+  }
+}
