a9774c1fcd364d96fd480deedb657f8081e8432b
Kevin Ascanio <kevinascanior@gmail.com>
2022-06-22 19:45:25 -0300

    Eventos prueba1.0

--- javas/miarchivo.js
+++ javas/miarchivo.js
@@ -1,23 +1,30 @@
-/////Ivan, tengo mis dudas en las siguientes lineas, por si me puedes ayudar con eso! Gracias!
-////// Lineas: 21, 111, 129
+const saludoForm = document.getElementById("formularioNombre");
+const saludoInicial = document.getElementById("ask-name");
+let nombre;
 
-// Este codigo de abajo es el desafio complementario!
-const saludoForm = document.getElementById("ask-name");
-const nombreformulario = document.getElementById("nombreFormulario");
-const apellidoformulario = document.getElementById("apellidoFormulario");
-
-formularioNombre.onsubmit = (e) => {
+saludoForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  saludoForm.innerHTML = `¡Hola! ${nombreformulario.value} ${apellidoformulario.value} Bienvenido. vamos a jugar piedra, papel o tijeras.`;
-};
 
-const comienzoJuego = () => {
-  if (nombre != "string" || nombre == " " || nombre == "") {
-    alert("No ingresaste un nombre valido. Intenta de nuevo.");
+  const nombreFormulario = e.target.children[0].value;
+  const apellidoFormulario = e.target.children[1].value;
+
+  if (
+    typeof nombreFormulario !== "string" ||
+    nombreFormulario === "" ||
+    nombreFormulario === " "
+  ) {
+    return alert("Ingrese un Nombre Correcto");
+  } else if (
+    typeof apellidoFormulario !== "string" ||
+    apellidoFormulario === "" ||
+    apellidoFormulario === " "
+  ) {
+    return alert("Ingrese un Apellido Correcto");
+  } else {
+    saludoInicial.innerHTML = `¡Hola! ${nombreFormulario} ${apellidoFormulario} Bienvenido. vamos a jugar piedra, papel o tijeras.`;
   }
-};
+});
 
-let nombre = document.getElementById("nombreFormulario").value;
 //intento capturar el valor de este formulario para, cuando el usuario lo complete, guardarlo en la variable nombre y que sirva para completar el objeto en la linea
 //220, pero no se porque no funciona!
 
@@ -90,167 +97,162 @@ console.log(jugadoresPremiados);
 let ganada = 0;
 let perdida = 0;
 let empatada = 0;
+let jugamos = 1;
+let eleccion;
+console.log(eleccion);
 
+///////////////FUNCIONES
 const suma = (m) => {
   return (m = m + 1);
 };
 
-let jugamos = true;
+const contar = () => {
+  const empecemos = document.getElementById("grito-inicio");
+  empecemos.innerHTML = `Okey aqui vamos.`;
 
-while (jugamos) {
-  function contar() {
-    alert("Okey aqui vamos.");
-    alert("Piedra, Papel o tijeras!");
+  const gritos = document.getElementById("grito-de-juego");
+  gritos.innerHTML = `Piedra, Papel o tijeras!`;
 
-    const empecemos = document.getElementById("grito-inicio");
-    empecemos.innerHTML = `Okey aqui vamos.`;
-
-    const gritos = document.getElementById("grito-de-juego");
-    gritos.innerHTML = `Piedra, Papel o tijeras!`;
-
-    for (h = 1; h <= 3; h++) {
-      // Aqui esto me imprime el 3 de una vez y no el conteo como con el alert. Me tocaria reemplazar esto por un array que recorra y se muestre?
-      const gritos = document.getElementById("grito-de-conteo");
-      gritos.innerHTML = h;
-    }
+  for (h = 1; h <= 3; h++) {
+    // Aqui esto me imprime el 3 de una vez y no el conteo como con el alert. Me tocaria reemplazar esto por un array que recorra y se muestre?
+    const gritos = document.getElementById("grito-de-conteo");
+    gritos.innerHTML = h++;
   }
-  const azarCompu = () => {
-    const elecciones = ["piedra", "papel", "tijeras"];
+};
+const azarCompu = () => {
+  const elecciones = ["piedra", "papel", "tijeras"];
 
-    const random = Math.round(Math.random() * 2);
+  const random = Math.round(Math.random() * 2);
 
-    let eleccionCompu = elecciones[random];
-    return eleccionCompu;
-  };
-  let opciones = 0;
-  opciones = document.querySelector('input[name="eleccion"]:checked').value;
-  console.log(opciones);
+  let eleccionCompu = elecciones[random];
+  return eleccionCompu;
+};
 
-  // Tengo duda de como capturar el valor de los radio buttons para capturar digamos el 1,2,3,4 o 5 y que me sirva para mis casos de switch. le asigne valores a cada boton
-  //pero no he logrado decifrarlo (he investigado por mi cuenta pero aun no lo logro)
+////////////////EMPEZAR BUCLE
 
-  /*
-  let opciones = Number(
-    prompt(`Haz tu eleccion: 
-    1.Piedra 
-    2.Papel 
-    3.Tijeras 
-    4.Ver Jugadores mas ganadores
-    5.No jugar`)
-  );
-  
-  */
+const formJugadaSeleccionada = document.getElementById("f2");
+while ((jugamos = 0)) {
+  formJugadaSeleccionada.onsubmit = (e) => {
+    e.preventDefault();
+    const radioEleccion = e.target.eleccion;
 
-  switch (true) {
+    radioEleccion.forEach((element) => {
+      if (element.checked) {
+        eleccion = element.value;
+
+        formJugadaSeleccionada.innerHTML = `Seleccionaste ${eleccion}`;
+      }
+    });
+
+    ////////////////JUEGO
     ////////CON USUARIO ELLECION PIEDRA
-    case opciones === 1 && azarCompu() === "papel":
-      contar();
-
-      alert(`Tu elegiste piedra y yo elegi papel jaja..Suerte la proxima.`);
-      perdida = suma(perdida);
-      break;
-
-    case opciones === 1 && azarCompu() === "piedra":
-      contar();
-
-      alert(`Tu elegiste piedra y yo elegi piedra jaja..Empate.`);
-      empatada = suma(empatada);
-      break;
-
-    case opciones === 1 && azarCompu() === "tijera":
-      contar();
-
-      alert(`Tu elegiste piedra y yo elegi tijera, me ganaste :(`);
-      ganada = suma(ganada);
-      break;
-
-    /////////CON USUARIO ELECCION PAPEL
-    case opciones === 2 && azarCompu() === "papel":
-      contar();
-      alert(`Tu elegiste papel y yo elegi papel jaja..Empate.`);
-      empatada = suma(empatada);
-      break;
-
-    case opciones === 2 && azarCompu() === "piedra":
-      contar();
-      alert(`Tu elegiste papel y yo elegi piedra, me ganaste :( `);
-      ganada = suma(ganada);
-      break;
-
-    case opciones === 2 && azarCompu() === "tijera":
-      contar();
-
-      alert(`Tu elegiste papel y yo elegi tijera jaja..Suerte la proxima.`);
-      perdida = suma(perdida);
-      break;
-
-    ///////////////CON USUARIO ELECCION TIJERA
-    case opciones === 3 && azarCompu() === "papel":
-      contar();
-      alert(`Tu elegiste tijeras y yo elegi papel, me ganaste :( `);
-      ganada = suma(ganada);
-      break;
-    case opciones === 3 && azarCompu() === "piedra":
-      contar();
-      alert(`Tu elegiste tijeras y yo elegi piedra jaja..Suerte la proxima. `);
-      perdida = suma(perdida);
-      break;
-
-    case opciones === 3 && azarCompu() === "tijera":
-      contar();
-      alert(`Tu elegiste tijeras y yo elegi tijeras Empate `);
-      empatada = suma(empatada);
-      break;
-
-    ///////////////CON USUARIO ELECCION DE VER PUNTUACIONES
-    case opciones === 4:
-      alert(
-        `Los jugadores mas ganadores son: ${jugadoresPremiados[0].nombre}, ${jugadoresPremiados[1].nombre}, ${jugadoresPremiados[2].nombre}, ${jugadoresPremiados[3].nombre}. Juega y únete a ellos!`
-      );
-      break;
-    ////////////////CON USUARIO QUERIENDO DEJAR EL JUEGO
-    case opciones === 5:
-      alert(`Entiendo ${nombre} , será en otro momento. Adios!`);
-      jugamos = false;
-      break;
-    ///////////////CON USUARIO ELECCION INCORRECTA
-    default:
-      //console.log("No elegiste un numero correcto, intenta de nuevo");
-      break;
-  }
-}
-
-///////////////TRABAJO EN PROGRESO
-
-const verResultado = () => {
-  class Usuario {
-    constructor(nombre, ganada, perdida, empatada) {
-      this.nombre = nombre;
-      this.ganadas = ganada;
-      this.perdida = perdida;
-      this.empatada = empatada;
+    const gritoResultado = document.getElementById("resultadoJuego");
+    if (eleccion === "piedra") {
+      if (azarCompu() === "papel") {
+        contar();
+        perdida = suma(perdida);
+        gritoResultado.innerHTML =
+          "Tu elegiste Piedra y yo elegi Papel ! Perdiste!";
+        S;
+      } else if (azarCompu() === "tijeras") {
+        contar();
+        ganada = suma(ganada);
+        gritoResultado.innerHTML =
+          "Tu elegiste Piedra y yo elegi Tijeras ! Ganaste!";
+      } else {
+        contar();
+        empatada = suma(empatada);
+        gritoResultado.innerHTML =
+          "Tu elegiste Piedra y yo elegi Piedra ! Empate!";
+      }
     }
+    ////////CON USUARIO ELLECION PAPEL
+    else if (eleccion === "papel") {
+      if (azarCompu() === "papel") {
+        contar();
+        empatada = suma(empatada);
+        console.log(empatada);
+        gritoResultado.innerHTML =
+          "Tu elegiste Papel y yo elegi Papel ! Empate!";
+      } else if (azarCompu() === "tijeras") {
+        contar();
+        perdida = suma(perdida);
+        console.log(perdida);
+        gritoResultado.innerHTML =
+          "Tu elegiste Papel y yo elegi Tijeras ! Perdiste!";
+      } else {
+        contar();
+        ganada = suma(ganada);
+        console.log(ganada);
+        gritoResultado.innerHTML =
+          "Tu elegiste Papel y yo elegi Piedra ! Ganaste!";
+      }
+    }
+    ////////CON USUARIO ELLECION TIJERAS
+    else if (eleccion === "tijeras") {
+      if (azarCompu() === "papel") {
+        contar();
+        ganada = suma(ganada);
+        gritoResultado.innerHTML =
+          "Tu elegiste Tijeras y yo elegi Papel ! Ganaste!";
+      } else if (azarCompu() === "tijeras") {
+        contar();
+        empatada = suma(empatada);
+        gritoResultado.innerHTML =
+          "Tu elegiste Tijeras y yo elegi Tijeras ! Empate!";
+      } else {
+        contar();
+        perdida = suma(perdida);
+        gritoResultado.innerHTML =
+          "Tu elegiste Tijeras y yo elegi Piedra ! Perdiste!";
+      }
+    }
+    ////////CON USUARIO ELECCION VER RANKING
+    else if (eleccion === "Ranking") {
+      gritoResultado.innerHTML = `Los jugadores mas ganadores son: ${jugadoresPremiados[0].nombre}, ${jugadoresPremiados[1].nombre}, ${jugadoresPremiados[2].nombre}, ${jugadoresPremiados[3].nombre}. Juega y únete a ellos!`;
+    }
+    ////////CON USUARIO ELLECION SALIR
+    else if (eleccion === "salir") {
+      const saludoForm = document.getElementById("formularioNombre");
 
-    mostrar() {
-      const resultado = document.createElement("h3");
-      resultado.innerHTML = `${nombre} tu resultado final fue: Partidas ganadas: ${ganada}, Partidas perdidas: ${perdida}, Partidas empatadas: ${empatada}. 
+      const nombreFormulario = saludoForm[0].value;
+      const apellidoFormulario = saludoForm[1].value;
+
+      gritoResultado.innerHTML = `Entiendo ${nombreFormulario} ${apellidoFormulario}, será en otro momento. Adios!`;
+
+      jugamos = 0;
+      console.log(jugamos);
+
+      ///////////////CONSTRUCTOR DE PARTIDAS
+
+      class Usuario {
+        constructor(nombre, ganada, perdida, empatada) {
+          this.nombre = nombre;
+          this.ganadas = ganada;
+          this.perdida = perdida;
+          this.empatada = empatada;
+        }
+
+        mostrar() {
+          const resultado = document.createElement("h3");
+          resultado.innerHTML = `${nombreFormulario} ${apellidoFormulario} tu resultado final fue: Partidas ganadas: ${ganada}, Partidas perdidas: ${perdida}, Partidas empatadas: ${empatada}. 
         
         Mejora tu puntuacion y juega de nuevo!`;
-      const resultadosFinales = document.getElementById("secondary-section");
-      resultadosFinales.append(resultado);
+          const resultadosFinales =
+            document.getElementById("secondary-section");
+          resultadosFinales.append(resultado);
+        }
+      }
+
+      const usuarioNuevo = new Usuario(nombre, ganada, perdida, empatada);
+
+      const historialPartidas = [];
+
+      historialPartidas.push(usuarioNuevo);
+
+      console.log(historialPartidas[0]);
+
+      usuarioNuevo.mostrar();
     }
-  }
-
-  const usuarioNuevo = new Usuario(nombre, ganada, perdida, empatada);
-
-  const historialPartidas = [];
-
-  historialPartidas.push(usuarioNuevo);
-
-  console.log(historialPartidas[0]);
-
-  usuarioNuevo.mostrar();
-};
-verResultado();
-
-/////////////////EVENTOS INPUTS
+  };
+}
